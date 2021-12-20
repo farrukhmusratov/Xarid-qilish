1c54ebd926f1b2773e2cd80f34d33efaa5496180
unknown <farruxmusratov@gmail.com>
2021-12-20 17:28:50 +0300

    delete xarid qilish js file

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-var list = document.querySelector('#list')
-
-button.addEventListener('click' , function (event){
-    var newLi = document.createElement('li')
-    newLi.setAttribute('class', 'list__item')
-    newLi.textContent = dataItem.value
-    list.appendChild(newLi)
-    dataItem.value = ''
-})
-
-
-dataItem.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("button").click();
-  }
-});
+// uzingiz yozing buyerda uyga vazifa uchirildi
